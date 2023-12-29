474abced42ccd1a02b1c2ab53f0c2e2c52b6969d
abdulmuid23 <130435385+abdulmuid23@users.noreply.github.com>
2023-12-29 22:10:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, I Love You!";
+  question.innerHTML = "Yay, I Love You! and now I am Mingle";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
